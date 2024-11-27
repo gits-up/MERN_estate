4bc513a4d7f4b712dca267767658eecb8570dc36
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2024-11-27 22:48:08 +0530

    Create user update api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
-import express from "express"; //type set as module in package.json so used import instead of require
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
+
 mongoose
-  .connect(process.env.MONGO) //personal note: check cloudflare for connecting (or any vpn, etc)
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log("DB connected");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
@@ -24,9 +28,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-//middleware to handle errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ success: false, statusCode, message });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
