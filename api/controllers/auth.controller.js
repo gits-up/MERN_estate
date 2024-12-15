d882a60859213d7e9036d17223a5a2e146869cb9
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2024-12-16 03:01:25 +0530

    Add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -68,3 +68,12 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("Signed Out Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
