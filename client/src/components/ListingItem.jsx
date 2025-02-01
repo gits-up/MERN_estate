62fee22a4c4a21f4104408c6694c65040bf83c55
Sayak Banerjee <sayakbanerjee155@gmail.com>
2025-02-02 00:22:49 +0530

    Update ListingItem.jsx

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -28,8 +28,8 @@ export default function ListingItem({ listing }) {
           <p className="text-slate-500 mt-2 font-semibold ">
             $
             {listing.offer
-              ? listing.discountPrice.toLocaleString("hi-IN")
-              : listing.regularPrice.toLocaleString("hi-IN")}
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
